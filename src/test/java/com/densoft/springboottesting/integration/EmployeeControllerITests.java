f3986d6e0f45e012e61e507432f53ae5316c9fee
dennis githinji <wakahiad@gmail.com>
2023-09-07 22:50:22 +0300

    create base for integration testing

---
+++ src/test/java/com/densoft/springboottesting/integration/EmployeeControllerITests.java
@@ -0,0 +1,30 @@
+package com.densoft.springboottesting.integration;
+
+import com.densoft.springboottesting.repository.EmployeeRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class EmployeeControllerITests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        employeeRepository.deleteAll();
+    }
+}
